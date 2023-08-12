c5da63807c8118341773400976210b9223e1d781
Queroixx <queirozgabriel660@gmail.com>
2023-08-11 22:10:25 -0300

    NotePage

--- NotePage.xaml.cs
+++ NotePage.xaml.cs
@@ -1,9 +1,31 @@
 namespace notes;
 
+
+
 public partial class NotePage : ContentPage
 {
-	public NotePage()
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public NotePage()
 	{
 		InitializeComponent();
+
+		if (File.Exists(_fileName))
+			TextEditor.Text = File.ReadAllText(_fileName);
 	}
+
+	private void SaveButton_Clicked(object sender, EventArgs e)
+	{
+		File.WriteAllText(_fileName, TextEditor.Text);
+
+	}
+
+	private void DeleteButton_Clicked(object sender, EventArgs e )
+	{
+		if (File.Exists(_fileName))
+			File.Delete(_fileName);
+
+		TextEditor.Text = string.Empty;
+	}
+	
 }
